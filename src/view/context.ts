5bec60ff21a123b8790e033d28d8ff1930282a8d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-17 17:52:15 +0300

    squash!

--- src/view/context.ts
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { VIEW_STATE } from "./constants";
-
-// View is running by default
-
-export const ViewStateContext = React.createContext(VIEW_STATE.RUNNING);
